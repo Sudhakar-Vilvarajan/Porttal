5e89cb118fa0ba8b24eeda7d2c3677ac628a668e
Subhendu Ghosh <subhendu.ghosh@jbhunt.com>
2020-11-03 10:58:42 -0600

    add code of pipes

--- src/app/layouts/quiz/pipes/timer.pipe.ts
+++ src/app/layouts/quiz/pipes/timer.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimerPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: number): string {
+    let h = Math.floor(value / 3600);
+    let m = Math.floor(value % 3600 / 60);
+    let s = Math.floor(value % 3600 % 60);
+
+    let hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+    let mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+    let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay;
   }
 
 }
