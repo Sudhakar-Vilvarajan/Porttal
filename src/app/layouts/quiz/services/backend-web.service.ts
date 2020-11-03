9624891e5632d27b5893db8f1c4b7d7c513ab32b
Subhendu Ghosh <subhendu.ghosh@jbhunt.com>
2020-11-03 10:56:20 -0600

    add code of services

--- src/app/layouts/quiz/services/backend-web.service.ts
+++ src/app/layouts/quiz/services/backend-web.service.ts
@@ -1,3 +1,6 @@
+import { Context } from './../context';
+import { BackendResponce, Team } from './../Interfaces';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +8,25 @@ import { Injectable } from '@angular/core';
 })
 export class BackendWebService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllTeams(){
+    return this.http.get<BackendResponce>(Context.webServiceRootPath + 'team');
+  }
+
+  addNewTeam(team : Team){
+    return this.http.post<BackendResponce>(Context.webServiceRootPath + 'team',team);
+  }
+
+  getTeamByID(teamID : number){
+    return this.http.get<BackendResponce>(Context.webServiceRootPath + 'team/' + teamID);      
+  }
+
+  getCoursesForTeamID(teamID : number){
+    return this.http.get<BackendResponce>(Context.webServiceRootPath + 'course?teamID=' + teamID);      
+  }
+
+  getQuestionsForCourseID(courseID : number){
+    return this.http.get<BackendResponce>(Context.webServiceRootPath + 'question?courseID=' + courseID);
+  }
 }
--- src/app/layouts/quiz/services/error.service.ts
+++ src/app/layouts/quiz/services/error.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 export class ErrorService {
 
   constructor() { }
+
+
+  private message : string;
+  
+  setMessage = (message : string)=>{
+      this.message = message;
+  }
+  getMessage = ()=>{
+      return this.message;
+  }
 }
