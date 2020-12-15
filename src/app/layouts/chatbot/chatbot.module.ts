0204a151347c3d016688282936d472e8769d7d7f
Subhendu Ghosh <subhendu.ghosh@jbhunt.com>
2020-12-15 05:52:18 -0600

    create required module, component and service

---
+++ src/app/layouts/chatbot/chatbot.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
+
+@NgModule({
+  declarations: [ChatDialogComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChatbotModule { }
