64f5d8f18d61023a8ebdcb34913686fef0fd94b4
rcon537 <sudhakar.vilvarajan@jbhunt.com>
2020-12-23 12:38:35 -0600

    changing the port number to default 8080

--- src/app/layouts/quiz/context.ts
+++ src/app/layouts/quiz/context.ts
@@ -1,4 +1,4 @@
 export class Context{
-    public static webServiceRootPath : string = 'http://localhost:9000/Quiz_Portal_WebService/';
+    public static webServiceRootPath : string = 'http://localhost:8080/Quiz_Portal_WebService/';
     public static defaultTeamBGImagePath : string = 'assets/broken.png';
 }
